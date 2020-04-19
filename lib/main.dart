44f47ac9503c923699408cd120a63428bb660cc4
deeks-codestar <deekshustle@gmail.com>
2020-04-19 01:18:01 -0700

    Diaplay 2 clickable dice, display random number on print

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -16,21 +17,33 @@ void main() {
 }
 
 class DicePage extends StatelessWidget {
+  // This can be placed outside since it need not be constructed again for every load of the page.
+  var r = Random.secure();
+
   @override
   Widget build(BuildContext context) {
+    var diceNum = 1;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset("images/dice1.png"),
+              // No need to wrap with Padding since the FlatButton is wrapped
+              // with padding of 16 by default. Can be checked in Flutter inspector.
+              child: FlatButton(
+                child: Image.asset("images/dice$diceNum.png"),
+                onPressed: () {
+                  diceNum = r.nextInt(6);
+                  print("Button 1 is pressed");
+                  print('Random is ${r.nextInt(6)}');
+                },
               )
           ),
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Image.asset("images/dice2.png"),
+              child: FlatButton(
+                child: Image.asset("images/dice$diceNum.png"),
+                onPressed: () {
+                  print("Button 2 pressed");
+                },
               )
           ),
         ],
